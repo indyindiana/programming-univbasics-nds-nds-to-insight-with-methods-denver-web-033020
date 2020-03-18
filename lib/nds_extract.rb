fce940cf851c3c74c7adc83fdd903926e752b46a
indiana danaher <indiana.danaher@gmail.com>
2020-03-18 00:08:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,17 @@ require 'directors_database'
 
 
 def gross_for_director(director_data)
+    director_gross = {}
   
+    movie_count = 0 
+    gross = 0 
+    while movie_count < directors_database[director_count][:movies].length do
+      gross += directors_database[director_count][:movies][movie_count][:worldwide_gross]
+      movie_count += 1 
+    end 
+    director_gross[directors_database[director_count][:name]] =  gross
+    
+
   
 end
 
